92c0ae14254328b28cb6474a5330428ae1ae6d46
marcelowkr2 <marcelowkr@hotmail.com>
2023-03-17 17:23:09 -0300

    atualizando rotas

--- src/routes/rotas.js
+++ src/routes/rotas.js
@@ -1,15 +1,48 @@
 const express = require('express');
 const pacientesController = require('../controllers/pacientes');
-const validate = require ('../middlewares/auth');
+const validatePacientes = require ('../middlewares/auth', '../validators/pacientes');
+
+const psicologosController = require("../controllers/psicologos");
+const atendimentosController = require('../controllers/atendimentos');
+const authController = require ("../controllers/authController");
+
+const psicologosValidation = require ("../validators/psicologos.js");
+const atendimentoValidation = require ("../validators/atendimentos.js");
+const authLoginValidation = require ("../validators/auth/login");
+const auth = require ("../middlewares/auth");
+
 const router = express.Router();
 
 
 //PACIENTES
 router.get('/pacientes', pacientesController.listar);
 router.get('/pacientes/:id', pacientesController.listarID);
-router.post('/pacientes/cadastrar', validate, pacientesController.cadastrar);
-router.put('/pacientes/:id', validate, pacientesController.atualizar);
-router.delete('/pacientes/:id',validate, pacientesController.deletar);
+router.post('/pacientes', validatePacientes, pacientesController.cadastrar);
+router.put('/pacientes/:id', validatePacientes, pacientesController.atualizar);
+router.delete('/pacientes/:id',validatePacientes, pacientesController.deletar);
+
+
+
+//PSICOLOGOS
+router.get("/psicologos", psicologosController.listar);
+router.get("/psicologos/:id", psicologosController.listarID);
+router.post("/psicologos",psicologosValidation, psicologosController.cadastrar);
+router.put("/psicologos/:id",psicologosValidation , psicologosController.atualizar);
+router.delete("/psicologos/:id", psicologosController.deletar)
+
+//ATENDIMENTOS
+router.get('/atendimentos', atendimentosController.listAtendimento);
+router.get('/atendimentos/:id', atendimentosController.listOne);
+router.post('/atendimentos',atendimentoValidation, auth, atendimentosController.createAtendimento);
+
+//DASHBOARD
+router.get('/dashboard/numero-paciente', pacientesController.countPacientes);
+router.get('/dashboard/numero-psicologo', psicologosController.countPsicologos);
+router.get('/dashboard/numero-atendimentos', atendimentosController.countAtendimentos);
+router.get('/dashboard/media-atendimentos', atendimentosController.averageAtendimentos);
+
+router.post("/login", authLoginValidation, authController.login);
+
 
 
 module.exports = router;
